f6c0eb96eb2b9b7a6f08122ced812a62795d60a4
Juan Victor Leite <jnvleite@gmail.com>
2022-05-06 17:48:12 -0300

    feat: post feedback and save on db

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,27 @@
 import express from 'express';
+import { prisma } from './prisma';
 
 const app = express();
 
-app.get('/users', (req, res) => res.send('opa\n'));
+app.use(express.json());
+
+app.post('/feedbacks', async (req, res) => {
+  const {
+    type,
+    comment,
+    screenshot,
+  } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  return res.send(res.status(201).json({ data: feedback }));
+});
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
