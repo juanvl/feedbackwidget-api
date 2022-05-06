e35d586cecdf70c7e688de4f3379936282de36ef
Juan Victor Leite <jnvleite@gmail.com>
2022-05-06 18:03:37 -0300

    feat: send mail with mailtrap

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
 import { prisma } from './prisma';
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '9fc50e5f38a5ba',
+    pass: '0c948e7bb1aedd',
+  },
+});
 
 app.post('/feedbacks', async (req, res) => {
   const {
@@ -18,6 +28,18 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot,
     },
+  });
+
+  await transport.sendMail({
+    from: 'Equipe Feedback Widget <oi@feedbackwidget.com>',
+    to: 'Juan Victor <jnvleite@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      '<div style="font-family: sans-serif; font-size: 16px; color: #111;">',
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      '</div>',
+    ].join('\n'),
   });
 
   return res.send(res.status(201).json({ data: feedback }));
