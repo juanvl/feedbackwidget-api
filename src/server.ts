9bb59be6d07854a5179ac5f337268d82ed132165
Juan Victor Leite <jnvleite@gmail.com>
2022-05-06 15:48:23 -0300

    build: prisma config sqlite

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,9 @@
-const teste = 1;
-console.log(teste);
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (req, res) => res.send('opa\n'));
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333');
+});
